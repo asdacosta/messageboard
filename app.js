34094a4ca8df3fe2dc78b5e5c5176a2cf00ce5ce
asdacosta <asdacosta18@gmail.com>
2024-10-17 17:27:31 +0000

    Make use of controllers

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("node:path");
 const router = express.Router();
 app.use(express.urlencoded({ extended: true }));
+const control = require("./controllers/control");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -10,74 +11,19 @@ app.set("view engine", "ejs");
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 
-const messages = [
-  {
-    text: "Nice express project!",
-    user: "Ace",
-    added: new Date(),
-  },
-  {
-    text: "Another time.",
-    user: "Nii",
-    added: new Date(),
-  },
-];
-
-app.get("/", (req, res) => {
-  res.render("index", { messages: messages });
-});
+app.get("/", control.renderMessages);
 
 router.get("/new", (req, res) => {
   res.render("form", { feedback: "" });
 });
 
-router.post("/new", (req, res) => {
-  const userName = req.body.name;
-  const userMessage = req.body.message;
+router.post("/new", control.postMessage);
 
-  if (userName === "" || userMessage === "") {
-    return res.render("form", { feedback: "Please fill in all fields!" });
-  }
-
-  messages.forEach((message) => {
-    if (userName === message.user) {
-      return res.render("form", { feedback: "Username already in use." });
-    }
-  });
-
-  messages.push({ text: userMessage, user: userName, added: new Date() });
-
-  res.redirect("/");
-});
-
-app.get("/:userName", (req, res, next) => {
-  const userName = req.params.userName;
-  let userMessage = null;
-  let date = null;
-  let foundUser = false;
-
-  if (userName === "new") return next();
-
-  messages.forEach((message) => {
-    if (message.user === userName) {
-      foundUser = true;
-      userMessage = message.text;
-      date = message.added;
-    }
-  });
-
-  if (!foundUser) res.status(404).send("User not found!");
-
-  res.render("detail", {
-    userName: userName,
-    userMessage: userMessage,
-    date: date,
-  });
-});
+app.get("/:userName", control.searchMessage);
 
 app.use("/", router);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Port ${PORT} ongoing.`);
 });
